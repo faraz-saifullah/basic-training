716db3d5931b42f0bdb694121819c3e773069be7
Faraz Saifullah <mansari@techracers.io>
2019-02-19 18:03:59 +0530

    Fixed a semicolon issue in line 4 of file assignment1_2.js

--- assignment1_2.js
+++ assignment1_2.js
@@ -1,6 +1,6 @@
 // Problem 2: Complete the calculateFrequency function that takes lowercase string as input and returns frequency of all english alphabet. (using only array, no in-built function)
 function calculateFrequency(string) {
-    string = string.replace(/\W/g, '')
+    string = string.replace(/\W/g, '');
     let myArray = string.split('');
     let myObject = {};
     for (let i = 0; i < myArray.length; i++) {
