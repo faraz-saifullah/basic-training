c298c72e9e4cdee9ad02c126e05bb8c14742c451
Faraz Saifullah <mansari@techracers.io>
2019-02-27 16:53:32 +0530

    Proper Formatting done

--- fileReadingCb.js
+++ fileReadingCb.js
@@ -5,15 +5,15 @@ arr[1] = 'file2.txt';
 arr[2] = 'file3.txt';
 arr[3] = 'file4.txt';
 (function read(i) {
-  fs.readFile(arr[i], function(err,data) {
-    if (err) {
-      console.log("Error reading file!\n");
-    } else {
-      console.log(data.toString());
-      console.log(data.toString());
-      console.log("===========");
-      if (i < 3)
-        read(i + 1);
-    }
-  })
+    fs.readFile(arr[i], function(err, data) {
+        if (err) {
+            console.log("Error reading file!\n");
+        } else {
+            console.log(data.toString());
+            console.log(data.toString());
+            console.log("===========");
+            if (i < 3)
+                read(i + 1);
+        }
+    })
 })(0);
