6ba0547291de5b58a0082c9ddc0b4b573047f405
Faraz Saifullah <mansari@techracers.io>
2019-02-27 17:18:57 +0530

    Proper indentation done

--- js/validations.js
+++ js/validations.js
@@ -1,69 +1,76 @@
-firstName.onblur = function errorMessage() {
-  if(firstName.value.length == 0)
-    errorMessageFirstName.style.display = 'block';
+firstName.onblur=function errorMessage() {
+  if(firstName.value.length==0) errorMessageFirstName.style.display='block';
 }
 
-firstName.onfocus = function hide() {
-  errorMessageFirstName.style.display = 'none';
+firstName.onfocus=function hide() {
+  errorMessageFirstName.style.display='none';
 }
 
-lastName.onblur = function errorMessage() {
-  if(lastName.value.length == 0)
-    errorMessageLastName.style.display = 'block';
+lastName.onblur=function errorMessage() {
+  if(lastName.value.length==0) errorMessageLastName.style.display='block';
 }
 
-lastName.onfocus = function hide() {
-  errorMessageLastName.style.display = 'none';
-}
-company.onblur = function errorMessage() {
-  if(company.value.length == 0)
-    errorMessageCompany.style.display = 'block';
+lastName.onfocus=function hide() {
+  errorMessageLastName.style.display='none';
 }
 
-company.onfocus = function hide() {
-  errorMessageCompany.style.display = 'none';
+company.onblur=function errorMessage() {
+  if(company.value.length==0) errorMessageCompany.style.display='block';
 }
-email.onblur = function errorMessage() {
-  let pattern =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(email.value.length == 0) {
-    errorMessageEmail.style.display = 'block';
-  } else if(!pattern.test(email.value)) {
-    errorInvalidEmail.style.display = 'block';
+
+company.onfocus=function hide() {
+  errorMessageCompany.style.display='none';
+}
+
+email.onblur=function errorMessage() {
+  let pattern=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2, 3})+$/;
+
+  if(email.value.length==0) {
+    errorMessageEmail.style.display='block';
+  }
+
+  else if( !pattern.test(email.value)) {
+    errorInvalidEmail.style.display='block';
   }
 }
 
-email.onfocus = function hide() {
-  errorMessageEmail.style.display = 'none';
-  errorInvalidEmail.style.display = 'none';
+email.onfocus=function hide() {
+  errorMessageEmail.style.display='none';
+  errorInvalidEmail.style.display='none';
 }
-areaCode.onblur = function errorMessage() {
+
+areaCode.onblur=function errorMessage() {
   // body...
-  if(areaCode.value.length == 0)
-    errorMessageAreaCode.style.display = 'block';
+  if(areaCode.value.length==0) errorMessageAreaCode.style.display='block';
 }
-areaCode.onfocus = function hide() {
-  errorMessageAreaCode.style.display = 'none';
+
+areaCode.onfocus=function hide() {
+  errorMessageAreaCode.style.display='none';
 }
-phone.onblur = function errorMessage() {
+
+phone.onblur=function errorMessage() {
   // body...
-  if(phone.value.length == 0)
-    errorMessagePhone.style.display = 'block';
+  if(phone.value.length==0) errorMessagePhone.style.display='block';
 }
-phone.onfocus = function hide() {
-  errorMessagePhone.style.display = 'none';
+
+phone.onfocus=function hide() {
+  errorMessagePhone.style.display='none';
 }
 
 function checkAgain() {
-  // alert('hey');
-  if(firstName.value.length == 0 || lastName.value.length == 0 || email.value.length == 0 || company.value.length == 0 || areaCode.value.length == 0 || phone.value.length == 0 || errorInvalidEmail.style.display == 'block') {
-    errorMessageFirstName.style.display = 'block';
-    errorMessageLastName.style.display = 'block';
-    errorMessageCompany.style.display = 'block';
-    errorMessageEmail.style.display = 'block';
-    errorMessageAreaCode.style.display = 'block';
-    errorMessagePhone.style.display = 'block';
 
-  } else {
+  // alert('hey');
+  if(firstName.value.length==0 || lastName.value.length==0 || email.value.length==0 || company.value.length==0 || areaCode.value.length==0 || phone.value.length==0 || errorInvalidEmail.style.display=='block') {
+    errorMessageFirstName.style.display='block';
+    errorMessageLastName.style.display='block';
+    errorMessageCompany.style.display='block';
+    errorMessageEmail.style.display='block';
+    errorMessageAreaCode.style.display='block';
+    errorMessagePhone.style.display='block';
+
+  }
+
+  else {
     document.getElementById('form').submit();
   }
 }
