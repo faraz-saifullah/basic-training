fcaee27cf5370f634088206091c3c11e127e69d7
Faraz Saifullah <mansari@techracers.io>
2019-02-27 17:11:36 +0530

    Proper indentation done

--- signup.js
+++ signup.js
@@ -1,15 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-http.createServer(function(request,response) {
+http.createServer(function(request, response) {
   var pathname = url.parse(request.url).pathname;
-  console.log('request for'+pathname+'received.');
-  fs.readFile(pathname.substr(1),function(err,data) {
+  console.log('request for' + pathname + 'received.');
+  fs.readFile(pathname.substr(1), function(err, data) {
     if (err) {
       console.log(err);
-      response.writeHead(404, {'Content-Type' :'text/html'});
+      response.writeHead(404, {
+        'Content-Type': 'text/html'
+      });
     } else {
-      response.writeHead(200, {'Content-Type' : 'text/html'});
+      response.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
       response.write(data.toString());
     }
     response.end();
