f82f7aca03223c42269d175c54d5f5e8ac9a621f
Faraz Saifullah <mansari@techracers.io>
2019-03-01 21:26:06 +0530

    APIs for signup and login properly working, not integrated with UI

--- myapp/myapp/app.js
+++ myapp/myapp/app.js
@@ -3,40 +3,42 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var engines = require('consolidate')
+var engines = require('consolidate');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+var connection = "postgres://postgres:rails@localhost:5432/test_my_api";
+var client = new pg.Client(connection);
+client.connect();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html',engines.mustache);
 app.set('view engine','html');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
--- myapp/myapp/routes/users.js
+++ myapp/myapp/routes/users.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var connection = "postgres://postgres:rails@localhost:5432/test_my_api";
+var client = new pg.Client(connection);
+client.connect();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id/:password', function(req, res, next) {
+  let id = parseInt(req.params.id);
+  let password = parseInt(req.params.password);
+  // console.log(id);
+  // console.log(req.params.password);
+  res.setHeader('Content-Type', 'text/html');
+  client.query('select * from userData where userid = $1 and password = $2;',[id,password],function(err,result) {
+    // console.log(typeof(result.rows[0]));
+    if(result.rows[0] == undefined) {
+      res.write('Invalid Credentials');
+    } else {
+      res.json(result.rows);
+    }
+
+    res.end();
+  });
+});
+
+//api for signup of new user
+router.post('/', function(req, res, next) {
+  // console.log('Post req parameters',req.body.firstname);
+  let userData = req.body;
+  let firstname = userData.firstname;
+  // console.log(typeof(firstname));
+  let lastname = userData.lastname;
+  // console.log(typeof(lastname));
+  let email = userData.email;
+  // console.log(typeof(email));
+  let password = userData.password;
+  // console.log(typeof(password));
+  let phone = Number(userData.phone);
+  // console.log(typeof(phone));
+  client.query('INSERT INTO userData (firstname, lastname, email, password, phone) VALUES ( $1, $2, $3, $4, $5)',[firstname, lastname, email, password, phone]);
+  res.send('Data written successfully!');
+  res.end();
 });
 
 module.exports = router;
