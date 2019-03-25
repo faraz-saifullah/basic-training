e781507b79bd5c96e349ef2755a74fac51735c30
Faraz Saifullah <mansari@techracers.io>
2019-03-25 13:58:37 +0530

    Error handling in session handling and proper .gitignore file added

--- myapp/myapp/routes/users.js
+++ myapp/myapp/routes/users.js
@@ -1,27 +1,26 @@
-var express = require('express');
-var router = express.Router();
-var Client = require('../app.js');
-var pg = require('pg');
-var connection = "postgres://postgres:rails@localhost:5432/test_my_api";
-var client = new pg.Client(connection);
+const express = require(`express`);
+const router = express.Router();
+const Client = require(`../app.js`);
+const pg = require(`pg`);
+const connection = `postgres://postgres:rails@localhost:5432/test_my_api`;
+const client = new pg.Client(connection);
 client.connect();
-router.get('/', function(req, res, next) {
-  res.setHeader('Content-Type', 'text/html');
-  let userData = req.params;
-  console.log(userData);
+router.get(`/`, function(req, res, next) {
+  res.setHeader(`Content-Type`, `text/html`);
+  let userData = req.query;
   let id = userData.usernameLogin;
   let password = userData.passwordLogin;
-  let Query = 'select * from userData where firstname = \''+id+'\' and password =\''+password+'\';';
+  let Query = `select * from userData where firstname = '${id}' and password ='${password}';`;
   client.query(Query,function(err,result) {
-    if(result == undefined) {
-      res.write('Invalid Credentials');
+    if(result.rowCount == 0) {
+      res.send(`Invalid Credentials`);
     } else {
-      res.render('info.html');
+      res.render(`info.html`);
       req.session.user = result.rows[0];
     }
   });
 });
-router.post('/', function(req, res, next) {
+router.post(`/`, function(req, res, next) {
   let userData = req.body;
   let firstname = userData.name;
   let lastname = userData.usernameSignup;
@@ -29,15 +28,18 @@ router.post('/', function(req, res, next) {
   let password = userData.passwordSignup;
   let phone = Number(userData.phone);
   client.query(`INSERT INTO userData (firstname, lastname, email, password, phone) VALUES ( '${firstname}', '${lastname}', '${email}', '${password}', ${phone});`);
-
-  res.render('index.html');
+  res.redirect(`/`);
 });
-router.get('/info', function(req, res, next) {
+router.get(`/info`, function(req, res, next) {
   if(!req.session.user) {
-    return res.send("Unauthorized");
+    return res.send(`Unauthorized`);
   } else {
     return res.json(req.session.user);
   }
+});
+router.get(`/logout`, function(req, res, next) {
+  req.session.destroy();
+  res.redirect(`/`);
 });
 
 module.exports = router;
